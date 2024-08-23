72f9c00ce406412325cc54d7e59a81105f193dd1
EvgenyRedko <binito1992@mail.ru>
2024-08-23 14:12:20 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(9 ** 0.5 * 5) #"1st program".
-print(9.99 >9.98 and 1000 != 1000.1) #'2nd program'.
-print(2 * 2 + 2 == 2 * (2 + 2 )) #'3rd program'.
-print(int(123.456 * 10 ) % 10) #'4th program'
+name = 'Urban'
+print(name[0])
+print(name[-1])
+print(name[2:])
+print(name[: : -1])
+print(name[1: :2])
