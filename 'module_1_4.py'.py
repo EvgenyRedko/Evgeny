524366892f730da14e006d0f5c13b0da01c0f129
EvgenyRedko <binito1992@mail.ru>
2024-08-25 17:30:48 +0300

    Add files via upload

---
+++ 'module_1_4.py'.py
@@ -0,0 +1,8 @@
+my_string = 'Всем привет, я ученик'
+number_of_characters = 17
+print(number_of_characters)
+print('Всем привет, я ученик'.upper())
+print('Всем привет, я ученик'.lower())
+print('Всем привет,я ученик'.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
